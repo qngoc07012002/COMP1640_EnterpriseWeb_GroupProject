925951ee59a5a1e6f9be3f4f6865729f1cb49c87
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2024-02-28 14:01:11 +0700

    Upload Project

---
+++ GreenwichUniversityMagazine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenwichUniversityMagazine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
