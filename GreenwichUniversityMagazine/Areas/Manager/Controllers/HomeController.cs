1c8b8eebb9d10e5323e36c01a436a7fce9084d9b
Ghauol123 <108039253+Ghauol123@users.noreply.github.com>
2024-03-06 16:29:48 +0700

    Areas (#1)

---
+++ GreenwichUniversityMagazine/Areas/Manager/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
+{
+    [Area("manager")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Manager/Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ GreenwichUniversityMagazine/Areas/Student/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenwichUniversityMagazine.Areas.Student.Controllers
+{
+    [Area("student")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Student/Views/Home/Index.cshtml");
+        }
+    }
+}
