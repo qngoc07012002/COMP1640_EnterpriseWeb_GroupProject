71c30cf20b5f2bf892e3641739015b93ec5bc8e8
Hieuu <110680846+hieucodegioi@users.noreply.github.com>
2024-03-11 06:06:00 +0700

    Update fix Login register

--- GreenwichUniversityMagazine/Areas/Student/Controllers/HomeController.cs
+++ GreenwichUniversityMagazine/Areas/Student/Controllers/HomeController.cs
@@ -31,19 +31,24 @@ namespace GreenwichUniversityMagazine.Areas.Student.Controllers
         public IActionResult Login(string email, string password)
         {
             User user = _unitOfWork.UserRepository.Login(email, password);
-            if (user != null)
+            if (user != null&& user.Password == password)
             {
-                HttpContext.Session.SetString("UserName", user.Name);
-                HttpContext.Session.SetString("UserId", user.Id.ToString());
-                TempData["success"] = "Login successful"; // Lưu thông báo đăng nhập thành công vào TempData
+                if (user.Password == password)
+                {
+                    HttpContext.Session.SetString("UserEmail", user.Email);
+                    HttpContext.Session.SetString("UserId", user.Id.ToString());
+                    /*TempData["success"] = "Login successful";*/ // Lưu thông báo đăng nhập thành công vào TempData
+                }
                 return RedirectToAction("Index", "Home", new { area = "student" });
             }
             else
             {
                 TempData["error"] = "Invalid email or password"; // Lưu thông báo lỗi vào TempData
-                return View();
+                ViewBag.Error = TempData["error"];
+                return View("Login");
             }
         }
+
 
 
 
@@ -54,23 +59,22 @@ namespace GreenwichUniversityMagazine.Areas.Student.Controllers
         [HttpPost]
         public IActionResult Register(User user)
         {
-            
             if (_unitOfWork.UserRepository.CheckEmail(user) == false)
             {
                 TempData["error"] = "Email Already Used";
-                return Register();
+                ViewBag.Error = TempData["error"]; // Thiết lập ViewBag.Error với giá trị của TempData["error"]
+                return View(user);
             }
             else
             {
                 _unitOfWork.UserRepository.Register(user);
-                TempData["success"] = "Account created succesfully";
-
-
+                TempData["success"] = "Account created successfully";
+                ViewBag.Success = TempData["success"]; // Thiết lập ViewBag.Success với giá trị của TempData["success"]
                 _unitOfWork.Save();
                 return RedirectToAction("Login");
             }
-
         }
+
         public IActionResult Logout()
         {
             HttpContext.Session.Remove("UserId");
