b1d07073f1ec8169574e9f528556e8815e938201
nguyentranazz123 <nguyenttgcd210485@fpt.edu.vn>
2024-03-17 17:26:33 +0700

    editprofileupdate

--- GreenwichUniversityMagazine/Areas/Student/Controllers/HomeController.cs
+++ GreenwichUniversityMagazine/Areas/Student/Controllers/HomeController.cs
@@ -1,7 +1,8 @@
 ﻿using GreenwichUniversityMagazine.Models;
 using GreenwichUniversityMagazine.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
-
+using GreenwichUniversityMagazine.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace GreenwichUniversityMagazine.Areas.Student.Controllers
 {
     [Area("student")]
@@ -28,32 +29,32 @@ namespace GreenwichUniversityMagazine.Areas.Student.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Login(string email, string password)
-        {
-            User user = _unitOfWork.UserRepository.Login(email, password);
-            if (user != null&& user.Password == password)
-            {
-                if (user.Password == password)
-                {
-                    HttpContext.Session.SetString("UserEmail", user.Email);
-                    HttpContext.Session.SetString("UserId", user.Id.ToString());
-                    if(user.Name!=null && user.avtUrl != null)
-                    {
-                        HttpContext.Session.SetString("UserName", user.Name);
-                        HttpContext.Session.SetString("avtUrl", user.avtUrl);
-                    }
-                  
-                    /*TempData["success"] = "Login successful";*/ // Lưu thông báo đăng nhập thành công vào TempData
-                }
-                return RedirectToAction("Index", "Home", new { area = "student" });
-            }
-            else
-            {
-                TempData["error"] = "Invalid email or password"; // Lưu thông báo lỗi vào TempData
-                ViewBag.Error = TempData["error"];
-                return View("Login");
-            }
-        }
+       public IActionResult Login(string email, string password)
+  {
+      User user = _unitOfWork.UserRepository.Login(email, password);
+      if (user != null&& user.Password == password)
+      {
+          if (user.Password == password)
+          {
+              HttpContext.Session.SetString("UserEmail", user.Email);
+              HttpContext.Session.SetString("UserId", user.Id.ToString());
+              if(user.Name!=null && user.avtUrl != null)
+              {
+                  HttpContext.Session.SetString("UserName", user.Name);
+                  HttpContext.Session.SetString("avtUrl", user.avtUrl);
+              }
+            
+              /*TempData["success"] = "Login successful";*/ // Lưu thông báo đăng nhập thành công vào TempData
+          }
+          return RedirectToAction("Index", "Home", new { area = "student" });
+      }
+      else
+      {
+          TempData["error"] = "Invalid email or password"; // Lưu thông báo lỗi vào TempData
+          ViewBag.Error = TempData["error"];
+          return View("Login");
+      }
+  }
 
 
 
@@ -85,10 +86,69 @@ namespace GreenwichUniversityMagazine.Areas.Student.Controllers
         {
             HttpContext.Session.Remove("UserId");
             HttpContext.Session.Remove("UserName");
-            HttpContext.Session.Remove("avtUrl");
-            HttpContext.Session.Remove("UserEmail");
-
             return RedirectToAction("Index", "Home", new { area = "student" });
         }
+        public IActionResult EditProfile()
+        {
+            int userId = int.Parse(HttpContext.Session.GetString("UserId"));
+            User user = _unitOfWork.UserRepository.Get(b => b.Id == userId);
+         
+            var faculties = _unitOfWork.FacultyRepository.GetAll();
+
+            // Create a new instance of UserVM and assign values
+            UserVM userVM = new UserVM
+            {
+                User = user,
+                MyFaculties = new SelectList(faculties, "Id", "Name", user.FacultyId)
+            };
+
+            return View(userVM);
+        }
+        [HttpPost]
+        public IActionResult EditProfile(IFormFile? file, UserVM userVM)
+        {
+            // Retrieve the User object from UserVM
+            User user = userVM.User;
+
+            // Check if the password is correct
+           
+                string wwwRoothPath = _webhost.WebRootPath;
+                if (file != null)
+                {
+                    string? fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                    // Delete Old Image
+                    var oldImagePath = user.avtUrl != null ? Path.Combine(wwwRoothPath, user.avtUrl.TrimStart('/')) : null;
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(wwwRoothPath, "img/avtImg/", fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    user.avtUrl = @"/img/avtImg/" + fileName;
+                }
+
+              
+
+                // Update User object
+                _unitOfWork.UserRepository.Update(user);
+                HttpContext.Session.SetString("avtUrl", user.avtUrl);
+                _unitOfWork.Save();
+                TempData["success"] = "Change Successfully";
+
+                // Retrieve the updated list of faculties
+                var faculties = _unitOfWork.FacultyRepository.GetAll();
+
+                // Update the MyFaculties property in UserVM
+                userVM.MyFaculties = new SelectList(faculties, "Id", "Name", user.FacultyId);
+            
+
+            // Return the view with the updated UserVM model
+            return View(userVM);
+        }
+
     }
 }
