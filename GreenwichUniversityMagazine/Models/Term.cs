72c2cf5d0e60d0ff42c90e0a22a45d287a8786a9
Irene Pain <hoanghuytran10102003@gmail.com>
2024-03-23 23:25:28 +0700

    Overview DashBoard for Manager
    using ajax to send request data for statistic visualization
    handle request by using query and controller

--- GreenwichUniversityMagazine/Models/Term.cs
+++ GreenwichUniversityMagazine/Models/Term.cs
@@ -8,5 +8,6 @@ namespace GreenwichUniversityMagazine.Models
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string Name {  get; set; }
     }
 }
--- GreenwichUniversityMagazine/Repository/IRepository/IArticleRepository.cs
+++ GreenwichUniversityMagazine/Repository/IRepository/IArticleRepository.cs
@@ -1,8 +1,14 @@
 ﻿using GreenwichUniversityMagazine.Models;
 using Microsoft.AspNetCore.Mvc;
+
 namespace GreenwichUniversityMagazine.Repository.IRepository
 {
     public interface IArticleRepository : IRepository<Article>
     {
+        IEnumerable<int> GetArticleCountsByTerm();
+        Task<IEnumerable<object>> GetMultipleLineChartData();
+        Task<IEnumerable<object>> GetGroupBarChart();
+        Task<IEnumerable<object>> GetStackBarChartContributions();
+
     }
 }
