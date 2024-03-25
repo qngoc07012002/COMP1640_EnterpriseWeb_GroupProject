dc83adc6aecd1b51ecf0fb5573f21bbbaa602129
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2024-03-26 06:41:03 +0700

    Revert "Merge branch 'master' into TTP"
    This reverts commit 4f0275c567e0118b02c5901e438ff73792554017, reversing
    changes made to 16c07b489df897cf3e9b553a4c4ba0db8520bd43.

--- GreenwichUniversityMagazine/Repository/IRepository/IArticleRepository.cs
+++ GreenwichUniversityMagazine/Repository/IRepository/IArticleRepository.cs
@@ -4,14 +4,5 @@ namespace GreenwichUniversityMagazine.Repository.IRepository
 {
     public interface IArticleRepository : IRepository<Article>
     {
-
-
-        public Article GetById(int id);
-
-        public IEnumerable<int> GetArticleCountsByTerm();
-        public Task<IEnumerable<object>> GetMultipleLineChartData();
-        public Task<IEnumerable<object>> GetGroupBarChart();
-        public Task<IEnumerable<object>> GetStackBarChartContributions();
-
     }
 }
