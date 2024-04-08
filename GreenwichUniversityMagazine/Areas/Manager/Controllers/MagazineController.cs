11f8d0a61bd1abb2e4c0a2ddd7b37ab3cb66d8dd
nguyentranazz123 <nguyenttgcd210485@fpt.edu.vn>
2024-04-08 17:23:49 +0700

    download zip file

--- GreenwichUniversityMagazine/Areas/Manager/Controllers/MagazineController.cs
+++ GreenwichUniversityMagazine/Areas/Manager/Controllers/MagazineController.cs
@@ -199,7 +199,7 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
         }
         public IActionResult ArticlesByMagazine(int? selectedMagazineId)
         {
-            var magazines = _unitOfWork.MagazineRepository.GetAll().ToList();
+            var magazines = _unitOfWork.MagazineRepository.GetAll().ToList().Where(b=>b.StartDate <= DateTime.Now);
             ViewBag.Magazines = new SelectList(magazines, "Id", "Title", selectedMagazineId);
 
             List<ArticleVM> articleVMs = new List<ArticleVM>();
@@ -207,7 +207,7 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
             if (selectedMagazineId != null)
             {
                 var articles = _unitOfWork.ArticleRepository.GetAll()
-                                .Where(a => a.MagazinedId == selectedMagazineId)
+                                .Where(a => a.MagazinedId == selectedMagazineId)  
                                 .ToList();
 
                 foreach (var article in articles)
@@ -224,9 +224,43 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
 
             return View(articleVMs);
         }
+        public IActionResult ArticlesByTerm(int? selectedTermId)
+        {
+            var terms = _unitOfWork.TermRepository.GetAll().ToList().Where(b => b.StartDate <= DateTime.Now);
+            ViewBag.Terms = new SelectList(terms, "Id", "Name", selectedTermId);
+
+            List<ArticleVM> articleVMs = new List<ArticleVM>();
+
+            if (selectedTermId != null)
+            {
+                var magazinesInTerm = _unitOfWork.MagazineRepository.GetAll()
+                                        .Where(m => m.TermId == selectedTermId)
+                                        .ToList();
+
+                foreach (var magazine in magazinesInTerm)
+                {
+                    var articles = _unitOfWork.ArticleRepository.GetAll()
+                                    .Where(a => a.MagazinedId == magazine.Id)
+                                    .ToList();
+
+                    foreach (var article in articles)
+                    {
+                        var articleVM = new ArticleVM
+                        {
+                            article = article,
+                        };
+                        articleVMs.Add(articleVM);
+                    }
+                }
+
+                return View(articleVMs);
+            }
+
+            return View(articleVMs);
+        }
+
         public byte[] GeneratePdfFromString(string text)
         {
-            // Loại bỏ các thẻ HTML và chuyển đổi các ký tự đặc biệt HTML
             string plainText = Regex.Replace(text, "<.*?>", string.Empty);
             plainText = System.Net.WebUtility.HtmlDecode(plainText);
 
@@ -289,7 +323,7 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
             return PhysicalFile(zipFilePath, "application/zip", $"article_{articleId}.zip");
         }
 
-        public IActionResult DownloadAllArticles(int selectedMagazineId)
+        public IActionResult DownloadAllArticlesByMagazine(int selectedMagazineId)
         {
             var articles = _unitOfWork.ArticleRepository.GetAll()
                 .Where(a => a.MagazinedId == selectedMagazineId && a.Status == true)
@@ -313,17 +347,17 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
                     {
                         var folderName = $"article_{article.ArticleId}";
                         var folderPathInArchive = folderName + "/";
-                        archive.CreateEntry(folderPathInArchive); // Tạo thư mục con cho mỗi bài báo
+                        archive.CreateEntry(folderPathInArchive); 
 
                         foreach (var filePath in Directory.GetFiles(articleFolderPath))
                         {
                             var entryName = Path.GetFileName(filePath);
                             var entryPathInArchive = folderPathInArchive + entryName;
-                            archive.CreateEntryFromFile(filePath, entryPathInArchive); // Tạo các entry trong thư mục con
+                            archive.CreateEntryFromFile(filePath, entryPathInArchive);
                         }
                     }
 
-                    var imageUrl = article.imgUrl; // Giả định rằng article.imgUrl là một chuỗi đường dẫn duy nhất
+                    var imageUrl = article.imgUrl; 
                     var imageName = Path.GetFileName(imageUrl);
                     var imagePhysicalPath = Path.Combine(_hostEnvironment.WebRootPath, imageUrl.TrimStart('/'));
                     if (System.IO.File.Exists(imagePhysicalPath))
@@ -354,6 +388,83 @@ namespace GreenwichUniversityMagazine.Areas.Manager.Controllers
 
             return PhysicalFile(zipFilePath, "application/zip", zipFileName);
         }
+        public IActionResult DownloadAllArticlesByTerm(int selectedTermId)
+        {
+            var magazinesInTerm = _unitOfWork.MagazineRepository.GetAll()
+                .Where(m => m.TermId == selectedTermId)
+                .ToList();
+
+            List<Article> articles = new List<Article>();
+
+            foreach (var magazine in magazinesInTerm)
+            {
+                var articlesInMagazine = _unitOfWork.ArticleRepository.GetAll()
+                    .Where(a => a.MagazinedId == magazine.Id && a.Status == true)
+                    .ToList();
+
+                articles.AddRange(articlesInMagazine);
+            }
+
+            var zipFileName = $"all_articles_of_term_{selectedTermId}.zip";
+            var zipFilePath = Path.Combine(_hostEnvironment.WebRootPath, "fileDownloaded", zipFileName);
+
+            if (System.IO.File.Exists(zipFilePath))
+            {
+                System.IO.File.Delete(zipFilePath);
+            }
+
+            using (var archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+            {
+                foreach (var article in articles)
+                {
+                    var articleFolderPath = Path.Combine(_hostEnvironment.WebRootPath, "Resource", "Article", article.ArticleId.ToString());
+
+                    if (Directory.Exists(articleFolderPath))
+                    {
+                        var folderName = $"article_{article.ArticleId}";
+                        var folderPathInArchive = folderName + "/";
+                        archive.CreateEntry(folderPathInArchive); 
+
+                        foreach (var filePath in Directory.GetFiles(articleFolderPath))
+                        {
+                            var entryName = Path.GetFileName(filePath);
+                            var entryPathInArchive = folderPathInArchive + entryName;
+                            archive.CreateEntryFromFile(filePath, entryPathInArchive);
+                        }
+                    }
+
+                    var imageUrl = article.imgUrl; 
+                    var imageName = Path.GetFileName(imageUrl);
+                    var imagePhysicalPath = Path.Combine(_hostEnvironment.WebRootPath, imageUrl.TrimStart('/'));
+                    if (System.IO.File.Exists(imagePhysicalPath))
+                    {
+                        using (var fileStream = new FileStream(imagePhysicalPath, FileMode.Open))
+                        {
+                            var entryPathInArchive = $"article_{article.ArticleId}/{imageName}";
+                            var entry = archive.CreateEntry(entryPathInArchive, CompressionLevel.Optimal);
+                            using (var entryStream = entry.Open())
+                            {
+                                fileStream.CopyTo(entryStream);
+                            }
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(article.Body))
+                    {
+                        var pdfBytes = GeneratePdfFromString(article.Body);
+                        var entryPathInArchive = $"article_{article.ArticleId}/article_{article.ArticleId}_body.pdf";
+                        var entry = archive.CreateEntry(entryPathInArchive, CompressionLevel.Optimal);
+                        using (var entryStream = entry.Open())
+                        {
+                            entryStream.Write(pdfBytes, 0, pdfBytes.Length);
+                        }
+                    }
+                }
+            }
+
+            return PhysicalFile(zipFilePath, "application/zip", zipFileName);
+        }
+
 
 
     }
